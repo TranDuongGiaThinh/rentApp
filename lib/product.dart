a5bd38d1769c4be10b63e64c68b54073d572cf19
Trần Dương Gia Thịnh <146810058+TranDuongGiaThinh@users.noreply.github.com>
2023-12-20 21:41:40 +0700

    add class product

--- lib/product.dart
+++ lib/product.dart
@@ -1,3 +1,17 @@
 class Product {
-  
+  int id;
+  String pathImage;
+  String name;
+  String price;
+  int bedrooms;
+  int bathroom;
+
+  Product({
+    required this.id,
+    required this.pathImage,
+    required this.name,
+    required this.price,
+    required this.bedrooms,
+    required this.bathroom,
+  });
 }
