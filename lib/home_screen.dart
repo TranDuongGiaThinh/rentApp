749099180574de7a2cdc374db00a89dedbc635e7
Trần Dương Gia Thịnh <146810058+TranDuongGiaThinh@users.noreply.github.com>
2023-12-20 21:44:17 +0700

    build homescreen

--- lib/home_screen.dart
+++ lib/home_screen.dart
@@ -1,4 +1,9 @@
 import 'package:flutter/material.dart';
+import 'package:mevivu_test/components/combobox.dart';
+import 'package:mevivu_test/components/list_near.dart';
+import 'package:mevivu_test/components/list_product.dart';
+import 'package:mevivu_test/components/nav.dart';
+import 'package:mevivu_test/components/search.dart';
 
 class HomeScreen extends StatefulWidget {
   const HomeScreen({super.key});
@@ -10,6 +15,38 @@ class HomeScreen extends StatefulWidget {
 class _HomeScreenState extends State<HomeScreen> {
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return Scaffold(
+      body: Padding(
+        padding: const EdgeInsets.all(10),
+        child: ListView(children: [
+          Row(
+            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+            children: [
+            const ComboBox(),
+            Stack(
+        children: [
+          const Icon(Icons.notifications_outlined),
+          Positioned(
+            right: 0,
+            top: 0,
+            child: Container(
+              width: 10,
+              height: 10,
+              decoration: const BoxDecoration(
+                color: Colors.red,
+                shape: BoxShape.circle,
+              ),
+            ),
+          ),
+        ],
+      ),
+          ],),
+          const Search(),
+          const Nav(),
+          ListNear(),
+          const ListProduct()
+        ]),
+      ),
+    );
   }
 }
