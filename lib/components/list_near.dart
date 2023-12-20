2ed96facaed8ca0c681010a615c30cc51dc89fd0
Trần Dương Gia Thịnh <146810058+TranDuongGiaThinh@users.noreply.github.com>
2023-12-20 21:44:42 +0700

    add listProduct

---
+++ lib/components/list_near.dart
@@ -0,0 +1,42 @@
+import 'package:flutter/material.dart';
+import 'package:mevivu_test/components/item_near.dart';
+import 'package:mevivu_test/product.dart';
+
+class ListNear extends StatelessWidget {
+  ListNear({super.key});
+
+  final List<Product> products = [
+    Product(
+        id: 1,
+        pathImage: "asset/img1.png",
+        name: "Orchad House",
+        price: "2.500.000.000",
+        bedrooms: 6,
+        bathroom: 4),
+    Product(
+        id: 2,
+        pathImage: "asset/img2.png",
+        name: "The Hollies House",
+        price: "2.000.000",
+        bedrooms: 5,
+        bathroom: 2),
+  ];
+
+  @override
+  Widget build(BuildContext context) {
+    return SingleChildScrollView(
+        child: Column(children: [
+          Row(mainAxisAlignment: MainAxisAlignment.spaceBetween, children: [
+            const Text("Near from you",
+                style: TextStyle(fontWeight: FontWeight.bold)),
+            GestureDetector(onTap: () {}, child: const Text("See more"))
+          ]),
+          Row(
+            children: [
+              for (Product product in products) ItemNear(product: product)
+            ],
+          )
+        ])
+    );
+  }
+}
---
+++ lib/components/list_product.dart
@@ -0,0 +1,30 @@
+import 'package:flutter/material.dart';
+import 'package:mevivu_test/components/item.dart';
+import 'package:mevivu_test/product.dart';
+
+class ListProduct extends StatefulWidget {
+  const ListProduct({super.key});
+
+  @override
+  State<ListProduct> createState() => _ListProductState();
+}
+
+class _ListProductState extends State<ListProduct> {
+  List<Product> products = [
+    Product(id: 1, pathImage: "asset/img1.png", name: "Orchad House", price: "2.500.000.000", bedrooms: 6, bathroom: 4),
+    Product(id: 2, pathImage: "asset/img2.png", name: "The Hollies House", price: "2.000.000", bedrooms: 5, bathroom: 2)
+  ];
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(children: [
+      Row(
+        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+        children:  [
+        const Text("Best for you", style: TextStyle(fontWeight: FontWeight.bold)),
+        GestureDetector(onTap: (){},child: const Text("See more"))
+      ]),
+      for(Product product in products) Item(product: product)
+    ],);
+  }
+}
